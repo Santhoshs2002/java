e2562cb14ee49ce2f52e09e46184dc6676446250
Santhoshs2002 <santhoshs.xworkz@gmail.com>
2024-06-26 00:27:53 +0530

    wed 26 june

---
+++ Reverse_array.java
@@ -0,0 +1,10 @@
+public class Reverse_array
+{
+public static void main(String[] args){
+int numbers[] = {1,2,3,4,5,6,}; 
+//int reverse = 0;
+for(int i=numbers.length-1 ; i>0  ; i-- ){
+System.out.println(" reverse is :"+numbers[i]+"");
+}
+}
+}
