9f7bc811a5ac62bf2a297c1dec2967f89b31090f
Santhoshs2002 <santhoshs.xworkz@gmail.com>
2024-07-08 10:16:00 +0530

    8th july

---
+++ ApplicationRunner (1).java
@@ -0,0 +1,12 @@
+public class ApplicationRunner{
+
+public static void main(String args[]){
+Application.job();
+//Application.email();
+long num[]={98765345498L};
+Application.email("samsung@gmail.com",num);
+Application.salary();
+String skills[] = {"Java","HTML","CSS","SQL"};
+Application.knowledge(skills,2);
+}
+}
