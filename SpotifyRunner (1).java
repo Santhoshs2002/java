b87c9b7e33fdd6d181ea850cc02a42059c3b3929
Santhoshs2002 <santhoshs.xworkz@gmail.com>
2024-07-11 09:43:07 +0530

    11th july

---
+++ SpotifyRunner (1).java
@@ -0,0 +1,19 @@
+public class SpotifyRunner{
+
+public static void main(String args[]){
+
+Spotify.name = "Nithin";
+Spotify.password = "nithin@3234";
+Spotify.phone = 9457638734L;
+Spotify.email = "nithin@gmail.com";
+Spotify.applanguage = "Kannada";
+System.out.println("Name is:"+Spotify.name);
+System.out.println("Password is:"+Spotify.password);
+System.out.println("Enter number is:"+Spotify.phone);
+System.out.println("Entered EmailID is:"+Spotify.email);
+System.out.println("App Language is:"+Spotify.applanguage);
+}
+
+
+
+}
