ce37b4b1f2bffd0642fdd180cec04edeca6bf3e5
Santhoshs2002 <santhoshs.xworkz@gmail.com>
2024-07-15 22:44:52 +0530

    16th july

---
+++ Constructer/Accenture.java
@@ -0,0 +1,28 @@
+public class Accenture{
+public String employe_name = "Dharshan";
+public byte age = 25;
+public int id_number = 2589;
+public short nuber_of_holidays = 5;
+public long phone_number =7896541235l;
+public float experience = 5.2f;
+public double salary = 30000.00d;
+public boolean isAvtive = true;
+public char gender = 'M';
+public String location = "Yealahanka";
+public String joining_date = "2023/12/25";
+public String employe_name1 = "Sahana";
+public byte age1 = 24;
+public int id_number1 = 8956;
+public short nuber_of_holidays1 = 2;
+public long phone_number1 =9517538524l;
+public float experience1 = 3.2f;
+public double salary1 = 25000.00d;
+public boolean isAvtive1 = true;
+public char gender1 = 'F';
+public String location1 = "BTM Layout";
+public String joining_date1 = "2022/07/3";
+
+public Accenture(){
+	System.out.println("ABOVE IS DETAILS OF 2 EMPLOYES");
+}
+}
